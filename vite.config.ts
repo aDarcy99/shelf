101fd39d3b15613ee04361d84519d364cf9eef0a
Lachlan Darcy <darcylachlan@hotmail.com.au>
2026-03-20 15:13:17 +1000

    update: WIP hosting on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/shelf/', // Must match github repo name to host on Github Pages
   plugins: [react()],
 });
