aaf3466d6b0af10da2576adb1c518bdd0e5c35ad
Lachlan Darcy <darcylachlan@hotmail.com.au>
2026-03-20 15:22:57 +1000

    fix: attempting to fix routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/shelf/', // Must match github repo name to host on Github Pages
+  base: '/',
   plugins: [react()],
 });
